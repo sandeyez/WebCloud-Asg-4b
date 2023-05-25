97184e34edea6c6b21ae757351c7c9e96acd003a
Max de Boer <max@deboer-vanvooren.nl>
2023-05-25 13:12:28 +0200

    Compute/Visualize werkt nog niet

--- compute/main.py
+++ compute/main.py
@@ -1,3 +1,87 @@
 #!/usr/bin/python3
+
+# CODE USED AS STARTING POINT: https://www.kaggle.com/code/gusthema/house-prices-prediction-using-tfdf/notebook
+
+import tensorflow as tf
+import tensorflow_decision_forests as tfdf
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+DATASET_PATH = 'data.csv'
+VARIABLE_IMPORTANCE_METRIC = 'NUM_AS_ROOT'
+
+
+# We import the training- and testing-dataset from the specified paths.
+data = pd.read_csv(DATASET_PATH)
+# We drop the 'Id' column because it is not a feature.
+df = data.drop(['Id'], axis=1)
+
+
+# We split the dataset into a training- and testing-dataset.
+# 80% of the data is used for training and the remaining 20% for testing.
+train_df = df.sample(frac=0.8, random_state=0)
+test_df = df.drop(train_df.index)
+print(f"Training set size: {len(train_df)}")
+print(f"Testing set size: {len(test_df)}")
+
+
+# Convert the pandas dataframe to a tensorflow dataset.
+# Because this is a regression problem, we specify the task as a regression task.
+# The label is the column 'SalePrice'. This is the value we want to predict.
+label = 'SalePrice'
+# complete_ds = tfdf.keras.pd_dataframe_to_tf_dataset(df, label=label, task=tfdf.keras.Task.REGRESSION)
+train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(train_df, label=label, task=tfdf.keras.Task.REGRESSION)
+test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(test_df, label=label, task=tfdf.keras.Task.REGRESSION)
+
+
+# Create a random forest model.
+random_forests = tfdf.keras.RandomForestModel(task=tfdf.keras.Task.REGRESSION)
+
+
+# Train the model.
+random_forests.fit(x=train_ds)
+
+
+random_forests.compile(metrics=["mse"])
+# evaluation = random_forests.evaluate(test_ds, return_dict=True)
+
+train_predictions = random_forests.predict(x=train_ds).flatten()
+print(train_predictions)
+print(train_predictions[0])
+test_predictions = random_forests.predict(x=test_ds).flatten()
+# complete_predictions = random_forests.predict(x=complete_ds)[0]
+
+train_actual_prices = np.array(train_df['SalePrice'])
+test_actual_prices = np.array(test_df['SalePrice'])
+
+train_merged = list(zip(train_predictions, train_actual_prices))
+test_merged = list(zip(test_predictions, test_actual_prices))
+
+# print(train_merged)
+# print(test_merged)
+
+
+# for name, value in evaluation.items():
+#   print(f"{name}: {value:.4f}")
+
+
 with open(f"/result/output.txt", 'w') as output_file:
-    output_file.write("Hello World!")
+    for pred, actual in train_merged:
+        output_file.write("{} {}\n".format(pred, actual))
+    output_file.write("\n")
+    for pred, actual in test_merged:
+        output_file.write("{} {}\n".format(pred, actual))
+
+# with open(f"/result/test.txt", 'w') as output_file:
+#     for pred, actual in test_merged:
+#         output_file.write("{} {}\n".format(pred, actual))
+
+# with open(f"/result/train.txt", 'w') as output_file:
+#     for pred, actual in train_merged:
+#         output_file.write("{} {}".format(pred, actual))
+
+# with open(f"/result/output.txt", 'w') as output_file:
+
--- visualization/main.py
+++ visualization/main.py
@@ -5,11 +5,25 @@ import yaml
 
 # Get the input file path from the environment variable
 print(os.environ, file=os.sys.stderr)
+
 input_file_path = f"{json.loads(os.environ['INPUT'])}/output.txt"
+
+# train_input_file_path = f"{json.loads(os.environ['TRAIN'])}/output.txt"
+# test_input_file_path = f"{json.loads(os.environ['TEST'])}/output.txt"
 
 # Read the input file
 with open(input_file_path, 'r') as input_file:
     input_file_content = input_file.read()
+
+
+# with open(train_input_file_path, 'r') as input_file:
+#     train_lines = [line.rstrip().split() for line in input_file]
+
+# with open(test_input_file_path, 'r') as input_file:
+#     test_lines = [line.rstrip().split() for line in input_file]
+
+# print(train_lines[0])
+# print(test_lines[0])
 
 # Print the input file content
 print(yaml.dump({"output": input_file_content}))
