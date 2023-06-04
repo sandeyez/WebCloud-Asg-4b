807cb4e4929fee8db5a2ddb038b703a7e453f84f
Max de Boer <max@deboer-vanvooren.nl>
2023-06-04 23:33:28 +0200

    Werkend + mooie plaatjes

--- compute/main.py
+++ compute/main.py
@@ -7,23 +7,18 @@ import json
 import tensorflow as tf
 import tensorflow_decision_forests as tfdf
 import pandas as pd
-# import seaborn as sns
-# import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
 # Redirect all stdout to stderr
 sys.stdout = sys.stderr
 
-
-# DATASET_PATH = 'data.csv'
 VARIABLE_IMPORTANCE_METRIC = 'NUM_AS_ROOT'
 
 # Get the input file path from the environment variable
 # print(os.environ, file=os.sys.stderr)
 
 input_file_path = f"{json.loads(os.environ['HOUSING'])}"
-# input_file_path = 'data.csv'
 
 # We import the training- and testing-dataset from the specified paths.
 data = pd.read_csv(input_file_path)
@@ -35,8 +30,6 @@ df = data.drop(['Id'], axis=1)
 # 80% of the data is used for training and the remaining 20% for testing.
 train_df = df.sample(frac=0.8, random_state=0)
 test_df = df.drop(train_df.index)
-# print(f"Training set size: {len(train_df)}")
-# print(f"Testing set size: {len(test_df)}")
 
 
 # Convert the pandas dataframe to a tensorflow dataset.
@@ -60,28 +53,13 @@ random_forests.compile(metrics=["mse"])
 
 inspector = random_forests.make_inspector()
 
-
-# for importance in inspector.variable_importances().keys():
-#   print("\t", importance)
-# for l in importances:
-#     print(importances[l])
-# print(importances)
-# evaluation = random_forests.evaluate(test_ds, return_dict=True)
-
 importances = inspector.variable_importances()['INV_MEAN_MIN_DEPTH']
 
 feature_names = [vi[0].name for vi in importances]
 feature_importances = [vi[1] for vi in importances]
 
-# print(feature_names)
-# print(feature_importances)
-
-
 train_predictions = random_forests.predict(x=train_ds).flatten()
-# print(train_predictions)
-# print(train_predictions[0])
 test_predictions = random_forests.predict(x=test_ds).flatten()
-# complete_predictions = random_forests.predict(x=complete_ds)[0]
 
 train_actual_prices = np.array(train_df['SalePrice'])
 test_actual_prices = np.array(test_df['SalePrice'])
@@ -90,14 +68,6 @@ train_merged = list(zip(train_predictions, train_actual_prices))
 test_merged = list(zip(test_predictions, test_actual_prices))
 
 features_merged = list(zip(feature_names, feature_importances))
-
-
-# print(train_merged)
-# print(test_merged)
-
-
-# for name, value in evaluation.items():
-#   print(f"{name}: {value:.4f}")
 
 # with open(f"output.txt", 'w') as output_file:
 with open(f"/result/output.txt", 'w') as output_file:
@@ -112,15 +82,4 @@ with open(f"/result/output.txt", 'w') as output_file:
 with open(f"/result/features.txt", 'w') as output_file:
     for name, importance in features_merged:
         output_file.write("{} {}\n".format(name, importance))
-
-
-# with open(f"/result/test.txt", 'w') as output_file:
-#     for pred, actual in test_merged:
-#         output_file.write("{} {}\n".format(pred, actual))
-
-# with open(f"/result/train.txt", 'w') as output_file:
-#     for pred, actual in train_merged:
-#         output_file.write("{} {}".format(pred, actual))
-
-# with open(f"/result/output.txt", 'w') as output_file:
 
--- visualization/main.py
+++ visualization/main.py
@@ -8,15 +8,9 @@ import numpy as np
 # Get the input file path from the environment variable
 # print(os.environ, file=os.sys.stderr)
 
-
 importances_file_path = f"{json.loads(os.environ['INPUT'])}/features.txt"
-# importances_file_path = 'features.txt'
 
 input_file_path = f"{json.loads(os.environ['INPUT'])}/output.txt"
-# input_file_path = 'output.txt'
-
-# train_input_file_path = f"{json.loads(os.environ['TRAIN'])}/output.txt"
-# test_input_file_path = f"{json.loads(os.environ['TEST'])}/output.txt"
 
 importances_data = []
 
@@ -33,7 +27,6 @@ with open(input_file_path, 'r') as input_file:
 
 
 idx = all_data.index([])
-# print(idx)
 
 train_data = all_data[:idx]
 test_data = all_data[idx + 1:]
@@ -44,42 +37,28 @@ feature_names = [vi[0] for vi in importances_data]
 feature_importances = [float(vi[1]) for vi in importances_data]
 feature_ranks = range(len(feature_names))
 
-# print(feature_names, feature_importances)
-
 complete_differences = np.clip([float(x[1]) - float(x[0]) for x in all_data], -100000, 100000)
 train_differences = np.clip([float(x[1]) - float(x[0]) for x in train_data], -100000, 100000)
 test_differences = np.clip([float(x[1]) - float(x[0]) for x in test_data], -100000, 100000)
 
-# From https://www.tensorflow.org/decision_forests/tutorials/advanced_colab
-# bar = plt.barh(feature_ranks, feature_importances, label=[str(x) for x in feature_ranks])
-# plt.yticks(feature_ranks, feature_names)
-# plt.gca().invert_yaxis()
-
-# for importance, patch in zip(feature_importances, bar.patches):
-#   importance = float(importance)
-#   plt.text(patch.get_x() + patch.get_width(), patch.get_y(), "{:.4f}".format(importance), va="top")
-
-# plt.xlabel('INV_MEAN_MIN_DEPTH')
-# plt.title("Don't really know")
-# plt.tight_layout()
-# plt.show()
-
-# print('We come here!')
-
+plt.figure(figsize=(18,12))
+plt.tick_params(axis='x', which='major', labelsize=11)
 plt.bar(feature_ranks, feature_importances, tick_label=feature_names)
 plt.title("Importance of all features")
 plt.xlabel('Feature name')
 plt.ylabel('Importance')
 plt.xticks(rotation='vertical')
-# plt.show()
 plt.savefig('/result/importances.png')
 
 
 
-fig = plt.figure()
+fig = plt.figure(figsize=(18, 12))
 ax1 = plt.subplot(131)
+ax1.tick_params(axis='x', which='major', rotation=30)
 ax2 = plt.subplot(132)
+ax2.tick_params(axis='x', which='major', rotation=30)
 ax3 = plt.subplot(133)
+ax3.tick_params(axis='x', which='major', rotation=30)
 ax1.hist(train_differences, bins=50, label='Train Data', density=True, lw=3, fc=(1, 0, 0, 1))
 ax1.title.set_text('Training data differences')
 ax2.hist(test_differences, bins=50, label='Test Data', density=True, lw=3, fc=(0, 1, 0, 1))
@@ -87,36 +66,6 @@ ax2.title.set_text('Test data differences')
 ax3.hist(complete_differences, bins=50, label='All Data', density=True, lw=3, fc=(0, 0, 1, 1))
 ax3.title.set_text('All data differences')
 fig.legend()
-# plt.show()
-# plt.hist(train_differences, bins=50, label='Train Data', density=True, lw=3, fc=(1, 0, 0, 0.5))
-# plt.hist(test_differences, bins=50, label='Test Data', density=True, lw=3, fc=(0, 1, 0, 0.5))
-# plt.hist(complete_differences, bins=50, label='All Data', density=True, lw=3, fc=(0, 0, 1, 0.5))
-# plt.xlabel('Difference between prediction and actual price')
-# plt.ylabel('Percentage of predictions')
-# plt.legend()
-# plt.show()
 plt.savefig('/result/differences.png')
 
 
-# train_pred = [x[0] for x in train_data]
-# train_real = [x[1] for x in train_data]
-
-# test_pred = [x[1] for x in test_data]
-# test_real = [x[1] for x in test_data]
-
-
-
-    # input_file_content = input_file.read()
-
-
-# with open(train_input_file_path, 'r') as input_file:
-#     train_lines = [line.rstrip().split() for line in input_file]
-
-# with open(test_input_file_path, 'r') as input_file:
-#     test_lines = [line.rstrip().split() for line in input_file]
-
-# print(train_lines[0])
-# print(test_lines[0])
-
-# Print the input file content
-# print(yaml.dump({"output": input_file_content}))
